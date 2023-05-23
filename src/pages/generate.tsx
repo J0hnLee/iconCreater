6e1495e26b40391dc53fee9f3b9e58fae417b422
Lee Cheng Hsun <john791016@gmail.com>
2023-05-23 20:31:39 +0800

    generate page

---
+++ src/pages/generate.tsx
@@ -0,0 +1,25 @@
+import { type NextPage } from "next";
+
+
+const Generate: NextPage = () => {
+
+  return (
+    <>
+     
+          
+       
+             
+         
+          <form className='flex flex-col'>
+            <div>Hello</div>
+            <label>prompt</label>
+            <input className=' bg-black'type="text"/>
+          </form>
+        
+    </>
+  );
+};
+
+export default Generate;
+
+
